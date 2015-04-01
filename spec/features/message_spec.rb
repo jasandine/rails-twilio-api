60b2d0abfb67567096f85105e0b5e00eead8a907
Joey Sandine and Sean Holstrom <jasandine@gmail.com>
2015-04-01 14:38:42 -0700

    Add test to verify user signup session

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Message, :vcr => true do
   it "doesn't save the message if twilio gives an error" do
     message = Message.new(:body => 'hi', :to => '1111111', :from => '5039463641')
-    message.save.should eq false
+    expect(message.save).to eq false
   end
 end
 
@@ -11,6 +11,6 @@ describe Message, :vcr => true do
   it 'adds an error if the to number is invalid' do
     message = Message.new(:body => 'hi', :to => '111111', :from => '9713402668')
     message.save
-    message.errors.messages[:base].should eq ["The 'To' number 111111 is not a valid phone number."]
+    expect(message.errors.messages[:base]).to eq ["The 'To' number 111111 is not a valid phone number."]
   end
 end
---
+++ spec/features/user_sessions_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe "the add a user process" do
+  it "adds a new user" do
+    user = FactoryGirl.build(:user)
+    visit new_user_registration_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password_confirmation
+    click_on "Join!"
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
+
+# describe "the sign out a user process" do
+#   let(:user) { FactoryGirl.create(:user) }
+#
+#   before do
+#     sign_in(user)
+#   end
+#
+#   it "signs out a user" do
+#     visit root_path
+#     click_on "Logout"
+#     expect(page).to have_content "Signed out successfully."
+#   end
+# end
+#
+# describe "the sign in a user process" do
+#     it "signs in a user" do
+#     user = FactoryGirl.create(:user)
+#     visit new_user_session_path
+#     fill_in "Username", with: user.username
+#     fill_in "Password", with: user.password
+#     click_on "Log in"
+#     expect(page).to have_content "Signed in successfully."
+#   end
+# end
